3e294ed7d6f748f96f10e4516d83fe7277464106
Daliso Joshua Ngoma <daliso@djngoma.com>
2017-05-19 03:19:10 +0800

    Updated to Vapor 2.0

--- Package.swift
+++ Package.swift
@@ -2,13 +2,17 @@ import PackageDescription
 
 let package = Package(
     name: "africantechno",
+    targets: [
+        Target(name: "App"),
+        Target(name: "Run", dependencies: ["App"]),
+    ],
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 5)
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
+        .Package(url: "https://github.com/vapor/leaf-provider.git", majorVersion: 1),
     ],
     exclude: [
         "Config",
         "Database",
-        "Localization",
         "Public",
         "Resources",
     ]
